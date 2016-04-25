ae0e3579230e85af2bdc5aff9fd2112e7df3c1a5
JayS <jrschwane@uwalumni.com>
2016-04-24 21:52:55 -0500

    Found and fixed todo description display bug

--- public/js/app.js
+++ public/js/app.js
@@ -402,37 +402,6 @@
         };
     });
     
-    app.directive('iosDblclick', function () {
-        
-        var DblClickInterval = 300,   // miliseconds
-            firstClickTime,
-            waitingSecondClick = false;
-        
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                element.bind('click', function (e) {
-                    
-                    if (!waitingSecondClick) {
-                        firstClickTime = (new Date()).getTime();
-                        waitingSecondClick = true;
-                        
-                        setTimeout(function () {
-                            waitingSecondClick = false;
-                        }, DblClickInterval);
-                    } else {
-                        waitingSecondClick = false;
-                        
-                        var time = (new Date()).getTime();
-						if (time - firstClickTime < DblClickInterval) {
-							scope.$apply(attrs.iosDblclick);
-						}
-					}
-				});
-			}
-		};
-	});
-    
     
     // =============================== FILTERS ================================
     
